d565a3c5affbd1efcf7dcaea2ab6b45e6873cec3
José Roberto Sudbrack <90429956+jrsudbrack@users.noreply.github.com>
2024-04-11 21:35:32 -0300

    Controller, DAOMentor, Print port listen
    Coloquei para printar a porta que está ouvindo;
    
    Inicio da DAOMentor
    criação da pasta Controller;

---
+++ database/DAOMentor.js
@@ -0,0 +1,14 @@
+const { Mentor } = require('../model/index');
+
+
+export default class Mentor {
+
+    static async insert(nome, email, senha, cpf, endereco, areaConhecimento) {
+        try{
+            
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+}
