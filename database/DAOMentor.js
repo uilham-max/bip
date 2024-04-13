f4089298cb5596ea6abad6db48729c615342d977
José Roberto Sudbrack <90429956+jrsudbrack@users.noreply.github.com>
2024-04-13 20:21:52 -0300

    Update DAOMentor.js

--- database/DAOMentor.js
+++ database/DAOMentor.js
@@ -5,7 +5,9 @@ export default class Mentor {
 
     static async insert(nome, email, senha, cpf, endereco, areaConhecimento) {
         try{
-            
+            let mentor = await Mentor
+                .insert({ nome: this.nome, email: this.email, senha: this.senha,
+                          cpf: this.cpf, endereco: this.endereco, areaConhecimento: this.areaConhecimento  });
         }catch(error){
             console.error(error);
         }
