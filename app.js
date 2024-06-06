fadbe4a832fde64ccbdb5256918751739eb41da1
José Roberto Sudbrack <90429956+jrsudbrack@users.noreply.github.com>
2024-06-05 21:31:09 -0300

    Criando primeira rota mentor

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var {Demandante, Problema, Estudante, Proposta, Projeto, Mentor, Etapa} = requir
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var demandanteRouter = require('./routes/demandanteRouter')
+const mentorRouter = require('./routes/mentorRouter');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/demandante', demandanteRouter);
+app.use('/mentor', mentorRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
