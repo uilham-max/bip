f0fb35cf18f63e62f1a8573f291690d9bc0fbf2f
José Roberto Sudbrack <90429956+jrsudbrack@users.noreply.github.com>
2024-06-13 21:01:01 -0300

    mentorController ajuste

--- controller/MentorController.js
+++ controller/MentorController.js
@@ -1,6 +1,26 @@
 
 
 
-exports.home = ((req, res) =>{
+exports.RenderHomePageMentor = ((req, res) =>{
+    res.send('200');
+});
+
+exports.RenderLogin = ((req, res) =>{
+    res.send('200');
+});
+
+exports.RenderCadastro = ((req, res) =>{
+    res.send('200');
+});
+
+exports.RealizarLogin = ((req, res) =>{
+    res.send('200');
+});
+
+exports.CadastrarBancoNovoMentor = ((req, res) =>{
+    res.send('200');
+});
+
+exports.ListarTodosMentor = ((req, res) =>{
     res.send('200');
 });
