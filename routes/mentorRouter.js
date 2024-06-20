d0c5e3c1dc90f162fbbb173707356273a2e87525
uilham-max <uilhamgoncalves@gmail.com>
2024-06-19 21:37:24 -0300

    Criadas pastas para problema

--- routes/mentorRouter.js
+++ routes/mentorRouter.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const MentorController = require('../controller/MentorController');
 
+<<<<<<< HEAD
 //router.get('/', MentorController.home)
+=======
+
+>>>>>>> acde72a (Criadas pastas para problema)
 
 module.exports = router;
