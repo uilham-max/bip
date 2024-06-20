bf8451a1b24328137904d9690d1a9754fa98b3a5
uilham-max <uilhamgoncalves@gmail.com>
2024-06-19 21:56:24 -0300

    Criadas problema controller

---
+++ controller/problemaController.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const DAOProblema = require('../database/DAOProblema')
+const { usuarioNome } = require('../helpers/getSessionNome')
+
+const getNovo = async (req, res) => {
+    res.render('problema/novo', {user: usuarioNome(req, res), mensagem:''})
+}
+
