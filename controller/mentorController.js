732bc79bbb6b330a2393cd76dfbcd78ad5193bc2
José Roberto Sudbrack <90429956+jrsudbrack@users.noreply.github.com>
2024-07-10 21:12:15 -0300

    Ajuste Cadastro Mentor

--- controller/mentorController.js
+++ controller/mentorController.js
@@ -6,23 +6,45 @@ exports.RenderHomePageMentor = ((req, res) =>{
 });
 
 exports.RenderLogin = ((req, res) =>{
-    res.send('200');
+    res.send('200')
 });
 
 exports.RenderCadastro = ((req, res) =>{
-    res.send('200');
+    res.render('mentor/novo');
+    return;
 });
 
 exports.RealizarLogin = ((req, res) =>{
-    res.send('200');
+    const mentor = DAOMentor.getOne({email: req.body.email})
+    if (mentor) {
+        req.session.usuario = {
+            id: estudante.id,
+            nome: estudante.nome,
+            email: estudante.email,
+            tipo: 'estudante',
+        };
+    console.log("Novo usuário na session:\n",req.session.usuario);
+    res.redirect('/');
+    } else {
+    res.render('estudante/login', {mensagem: 'Usuário ou senha inválidos.'});
+    }
 });
 
 exports.CadastrarNovoMentor = ( async (req, res) =>{
-    let {nome, email, senha, cpf, endereco, areaConhecimento} = req.body;
+    let {nome, email, senha, cpf, cep, rua, numeroDaCasa, complemento, localidade, uf, areaConhecimento} = req.body;
+    let endereco = {
+        rua: rua,
+        numeroDaCasa: numeroDaCasa,
+        complemento: complemento,
+        cep: cep,
+        localidade: localidade,
+        uf: uf
+    }
     let validator = validaCadastroMentor(nome, email, senha, cpf, endereco, areaConhecimento);
     if (validator[0]){
+        let enderecoString = JSON.stringify(endereco);
         try{
-            await DAOMentor.insert(nome, email, senha, cpf, endereco, areaConhecimento);
+            await DAOMentor.insert(nome, email, senha, cpf, enderecoString, areaConhecimento);
             res.status(200)
             res.send('Mentor created!')
         }catch(e){
